9b3c7066a955e117b197bee39fc4478655140a28
Prerna <psurbhi@lb.com>
2020-05-17 20:29:15 +0530

    change the app icon of both android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ void main() {
         backgroundColor: Color(0xff84FFFF) ,
         body:Center(
           child: Image(
-            image:NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRKcLEDIbwUwh9hpuA3U1eFkn0JVUnlcC6WTucP4yodBlcpp5Ye&usqp=CAU'),
+//            image:NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRKcLEDIbwUwh9hpuA3U1eFkn0JVUnlcC6WTucP4yodBlcpp5Ye&usqp=CAU'),
+           image:AssetImage('images/diamond.png')
           ),
         ),
         appBar: AppBar(
