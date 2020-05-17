2dacd9f3178df3b923ee257554cc0ab6cf53144d
Prerna <psurbhi@lb.com>
2020-05-17 17:20:33 +0530

    added a center image on body . added image in newly created directory images . image name is diamond.png.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Smile Prerna'),
+      home: Scaffold(
+        backgroundColor: Color(0xff84FFFF) ,
+        body:Center(
+          child: Image(
+            image:NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRKcLEDIbwUwh9hpuA3U1eFkn0JVUnlcC6WTucP4yodBlcpp5Ye&usqp=CAU'),
+          ),
+        ),
+        appBar: AppBar(
+          title: Center(child: Text("Live Chat"),
+          ),
+          backgroundColor: Colors.blueGrey,
+        ) ,
       ),
     ),
   );
